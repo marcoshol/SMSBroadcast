d059655f0bb38ef6cb412e9cc43c6c63f9b316b8
aluno <aluno@aluno.com>
2015-05-04 10:31:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aluno/sms/SMSBC.java
@@ -0,0 +1,18 @@
+package com.example.aluno.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SMSBC extends BroadcastReceiver {
+    public SMSBC() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
